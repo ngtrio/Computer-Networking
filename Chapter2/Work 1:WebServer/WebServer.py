bf699e9bd3bf83c957c6c4373fd1fd839f6aa813
Jing <985035177@qq.com>
2018-09-05 00:19:11 +0800

    work 1:web server

--- Chapter2/Work 1:WebServer/WebServer.py
+++ Chapter2/Work 1:WebServer/WebServer.py
@@ -1,7 +1,7 @@
 from socket import *
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverPort = 10000
+serverPort = 10086
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 while True:
@@ -9,16 +9,15 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     try:
         message = connectionSocket.recv(2048).decode()
-        print(message)
         filename = message.split()[1]   # 请求的路径
         f = open(filename[1:])          # 请求的文件名
         outputdata = f.read()
-        header = 'HTTP/1.1 200 OK\nContent-Type: text/html'
+        header = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8'
         connectionSocket.send(header.encode())
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i])
+        # for i in range(0, len(outputdata)):
+        connectionSocket.send(outputdata.encode())
         connectionSocket.close()
     except IOError:
-        header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html'
+        header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=utf-8'
         connectionSocket.send(header.encode())
         connectionSocket.close()
