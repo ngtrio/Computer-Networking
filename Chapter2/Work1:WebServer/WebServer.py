de64eeb2949c916bfa314c943a9d950aa2d35002
Jing <985035177@qq.com>
2018-09-06 17:32:26 +0800

    update

--- Chapter2/Work1:WebServer/WebServer.py
+++ Chapter2/Work1:WebServer/WebServer.py
@@ -3,8 +3,10 @@ from socket import *
 import sys
 import _thread
 
+
 def getFileName(request):
     return request.split()[1][1:]
+
 
 def myThread(connectionSocket, addr):
     request = connectionSocket.recv(2048)
@@ -12,15 +14,16 @@ def myThread(connectionSocket, addr):
     try:
         with open(fileName, 'r') as f:
             outPutData = f.read()
-            connectionSocket.send('''HTTP/1.1 200 OK\r\n
-                                     Content-Type: text/html\r\n'''.encode())
-            connectionSocket.send(outPutData.encode())
+            header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+            responce = header + outPutData
+            connectionSocket.send(responce.encode())
         print('{} Request success'.format(addr))
     except IOError:
         print('{} request fail'.format(addr))
-        connectionSocket.send('HTTP/1.1 404 NOT FOUND\r\n'.encode())
+        connectionSocket.send('HTTP/1.1 404 NOT FOUND\r\n'.encode('utf-8'))
     finally:
         connectionSocket.close()
+
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = int(sys.argv[1])
@@ -30,4 +33,3 @@ print('The server is ready to receive...')
 while True:
     connectionSocket, addr = serverSocket.accept()
     _thread.start_new_thread(myThread, (connectionSocket, addr))
-
